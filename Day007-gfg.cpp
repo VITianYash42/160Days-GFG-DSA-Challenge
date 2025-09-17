98a64b468575816db94fe31720723a3e7b2a17dc
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2025-09-17 21:08:28 +0530

    Solution for Day 7

---
+++ Day007-gfg.cpp
@@ -0,0 +1,26 @@
+                                          // DAY 8 - "STOCK BUY AND SELL - MULTIPLE TRANSACTIONS ALLOWED" //
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+  public:
+    int maximumProfit(std::vector<int> &prices) {
+        // code here
+        int total_profit = 0; 
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i - 1]) {
+                total_profit += prices[i] - prices[i-1];
+            }
+        }
+        return total_profit;
+    }
+};
+
+int main() {
+    Solution S;
+    std::vector<int> test_prices = {10, 20, 5, 15};
+    int profit = S.maximumProfit(test_prices);
+    std::cout << "Maximum profit: " << profit << std::endl;
+    return 0;
+}
